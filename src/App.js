74ebe7b60b6c19a9ce5589a0936545af2fd264ce
David Abol Ger <draker74@gmail.com>
2022-09-16 01:04:38 +0300

    Add a css linters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Greeting from './components/greetings';
+import './ass.css';
 
 function App() {
   return (
--- src/components/greetings.js
+++ src/components/greetings.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import '../ass.css';
 
 const Greeting = () => {
   const greetings = useSelector((state) => state.greetingReducer);
